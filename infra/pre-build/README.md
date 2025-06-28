55b57cb2264a71b18d0d71fd194463e1f12bed1a
Juan A <jdarguello@MacBook-Pro-de-Juan.local>
2025-06-28 11:15:04 -0500

    infra: estructura pre-build

---
+++ infra/pre-build/README.md
@@ -0,0 +1,3 @@
+# Preparación de IaC
+
+Gestión de recursos preliminares para la persistencia de la estrategia de IaC. Habilitaremos un Bucket S3 para almacenar las versiones de infraestructura.
---
+++ infra/pre-build/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">=1.5"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = ">=5.3"
+    }
+  }
+
+  backend "s3" {
+    
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
